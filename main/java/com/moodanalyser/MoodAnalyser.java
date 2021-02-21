89c4e0ceef3b84683ac6c4a1c54c63fc536c7fb3
viresh rawool <vireshrawool007@gmail.com>
2021-02-21 22:27:43 +0530

    added method for handling null exception

--- main/java/com/moodanalyser/MoodAnalyser.java
+++ main/java/com/moodanalyser/MoodAnalyser.java
@@ -4,7 +4,11 @@ public class MoodAnalyser {
 
     public String message;
 
-    public String analyseMood(String message){
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood(String message) {
         if (message.contains("Sad")) {
             return "SAD";
         } else {
@@ -12,4 +16,15 @@ public class MoodAnalyser {
         }
     }
 
+    public String analyseMood() {
+        try {
+            if (message.contains("Sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        }catch (NullPointerException e){
+            return "HAPPY";
+        }
+    }
 }
--- test/java/com/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -8,18 +8,23 @@ public class MoodAnalyserTest {
 
     @Test
     public void givenMessage_whenSad_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("this is a Sad Message");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("this is a Sad Message");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
     public void givenMessage_whenNotSad_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("this is a Happy Message");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("this is a Happy Message");
+        String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
-
+    @Test
+    public void givenNullMoodShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
 }
 
 
